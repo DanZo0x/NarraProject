6f0e96358e83dd487a36b607975051a2df37bb02
Emile Birraux <Emile.birraux@edu.devinci.fr>
2024-01-25 16:34:22 +0100

    [ADD]Proto de merde sur la Mainscene

--- Assets/Scripts/Annuaire.cs
+++ Assets/Scripts/Annuaire.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Annuaire : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    bool _isOpenning = false;
+    bool _isClosing = false;
+    Vector2 _currentScale;
+    RectTransform _rect;
+    float _timer=0;
+    private void Awake()
     {
-        
+        _rect = GetComponent<RectTransform>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (_isOpenning)
+        {
+           _rect.anchorMin= Vector2.Lerp(_rect.anchorMin, new Vector2(0.5f, 0.5f), _timer/3);
+           _rect.anchorMax= Vector2.Lerp(_rect.anchorMax, new Vector2(0.5f, 0.5f), _timer/3);
+           _rect.anchoredPosition= Vector2.Lerp(_rect.anchoredPosition, new Vector2(0, 0), _timer/3);
+            _rect.localScale = Vector2.Lerp(_rect.localScale, _currentScale * 2, _timer / 3);
+            _timer += Time.deltaTime;
+        }
+        if (_isClosing)
+        {
+            _rect.anchorMin = Vector2.Lerp(_rect.anchorMin, new Vector2(0, 0.5f), _timer / 3);
+            _rect.anchorMax = Vector2.Lerp(_rect.anchorMax, new Vector2(0, 0.5f), _timer / 3);
+            _rect.anchoredPosition = Vector2.Lerp(_rect.anchoredPosition, new Vector2(50, 0), _timer / 3);
+            _rect.localScale = Vector2.Lerp(_rect.localScale, _currentScale / 2, _timer / 3);
+            _timer += Time.deltaTime;
+        }
+        if (_timer > 3)
+        {
+            _isClosing = false;
+            _isOpenning = false;
+            _timer = 0;
+        }
+    }
+    public void Open()
+    {
+        _currentScale = _rect.localScale;
+        _isOpenning = true;
+        _isClosing = false;
+        _timer = 0;
+    }
+    public void Close()
+    {
+        _currentScale = _rect.localScale;
+        _isOpenning = false;
+        _isClosing = true;
+        _timer = 0;
     }
 }
