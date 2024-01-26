ff8b4f012fd82da0a3d5cf89b6d0c699061feb65
Alexis Le Bourhis <lebourhisalexis11@gmail.com>
2024-01-26 09:43:07 +0100

    ADD|DialogSystem

--- Assets/NodeBasedDialogueSystem-master/com.subtegral.dialoguesystem/Editor/Nodes/DialogueNode.cs
+++ Assets/NodeBasedDialogueSystem-master/com.subtegral.dialoguesystem/Editor/Nodes/DialogueNode.cs
@@ -4,12 +4,15 @@ using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
+
 namespace Subtegral.DialogueSystem.Editor
 {
     public class DialogueNode : Node
     {
+        public 
         public string DialogueText;
         public string GUID;
         public bool EntyPoint = false;
     }
 }
+
